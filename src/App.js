83eff88b16fa6194bd4f24a538ab290aed8fb8fa
MacAttack79 <jmcintosh1979@gmail.com>
2019-01-16 13:24:21 -0500

    removed a function that wasn't needed

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import TransformersCard from './components/TransformersCard';
 import Footer from './components/Footer';
 
 
-function shuffleArray(array) {
-  let i = array.length - 1;
-  for (; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-}
-
-
 class App extends Component {
   // Setting the state for images and score
   state = {
